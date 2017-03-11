fa0199461836f61040f2b0d3f83b2f96090d7be4
Sean <sean.r.innes@googlemail.com>
2017-03-11 00:44:53 +0000

    Altered init

--- apis/reddit.py
+++ apis/reddit.py
@@ -7,7 +7,10 @@ reddit = praw.Reddit(client_id='QNODIY8pI3SAHw',
                      user_agent='Sending messages (to people that want them) by /u/BombScheduling, StudentHackV project')
 
 class RedditMessage():
-    def __init__(self, reddit_username, reddit_message_title, reddit_message_body):
+    def __init__(self):
+        return None
+
+    def create(self, reddit_username, reddit_message_title, reddit_message_body):
         self.reddit_username      = reddit_username
         self.reddit_message_title = reddit_message_title
         self.reddit_message_body  = reddit_message_body
@@ -15,8 +18,9 @@ class RedditMessage():
 def send(msg):
     reddit.redditor(msg.reddit_username).message(msg.reddit_message_title, msg.reddit_message_body)
 
-# def test():
-#     msg = RedditMessage("evilpenguinsinspace", "Howdy", "Hello, World!")
-#     send(msg)
-#
-# test()
+def test():
+    msg = RedditMessage()
+    msg.create("evilpenguinsinspace", "Howdy", "Hello, World!")
+    send(msg)
+
+test()
