ac74a828f4124d3cc52fa1b65478e19b783972ed
Raef Coles <Cretbo@gmail.com>
2017-03-11 01:09:04 +0000

    Update to create objects

--- apis/reddit.py
+++ apis/reddit.py
@@ -7,17 +7,14 @@ reddit = praw.Reddit(client_id='QNODIY8pI3SAHw',
                      user_agent='Sending messages (to people that want them) by /u/BombScheduling, StudentHackV project')
 
 class RedditMessage():
-    reddit_username      = ""
-    reddit_message_title = ""
-    reddit_message_body  = ""
 
     def __init__(self):
         return None
-
-    # def create(self, reddit_username, reddit_message_title, reddit_message_body):
-        # self.reddit_username      = reddit_username
-        # self.reddit_message_title = reddit_message_title
-        # self.reddit_message_body  = reddit_message_body
+    
+    def create(self):
+        self.reddit_username      = ""
+        self.reddit_message_title = ""
+        self.reddit_message_body  = ""
 
 def send(msg):
     reddit.redditor(msg.reddit_username).message(msg.reddit_message_title, msg.reddit_message_body)
