e13e14e283b40c652a8469efd13500e2b28875f3
Raef Coles <Cretbo@gmail.com>
2017-03-12 02:33:57 +0000

    Add stuff to client

--- client/client.py
+++ client/client.py
@@ -5,15 +5,17 @@ import time
 import sys
 server = "ws://139.59.162.84:40111"
 filename = "uid.txt"
+flags_start = 1
 
 if len(sys.argv) == 1:
     print ("MORE ARGS")
 else:
-    if len(sys.argv) == 3:
-        if sys.argv[2] == "debug":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "debug":
+            flags_start += 1
             server = "ws://localhost:40111"
             filename = "debug_uid.txt"
-    if sys.argv[1] == "register":
+    if sys.argv[flags_start] == "register":
         ws = create_connection(server)
         print("hah")
         print ("REQ")
@@ -35,7 +37,7 @@ else:
         print (uid)
         uidfile = open(filename, "w+")
         uidfile.write(str(uid))
-    elif sys.argv[1] == "schedule":
+    elif sys.argv[flags_start] == "schedule":
         ws = create_connection(server)
         try:
             uidfile = open(filename, "rt")
@@ -67,14 +69,28 @@ else:
         now = time.time()
 
         d = {}
-        d["title"] = "Hey Sean"
-        d["message"] = "It works"
-        d["time"] = now
-        d["uid"] = uid
+        if len(sys.argv) == flags_start + 4:
+            d["title"] = sys.argv[flags_start + 1]
+            d["message"] = sys.argv[flags_start + 2]
+            t = sys.argv[flags_start + 3]
+            if t[0] == "+":
+                print(t[1:])
+                d["time"] = now + float(t[1:])
+            else:
+                d["time"] = float(t)
+            d["uid"] = uid
+        else:
+            d["title"] = ""
+            d["message"] = "test"
+            d["time"] = now
+            d["uid"] = uid
 
         ws.send("BMB" + json.dumps(d))
+        print(ws.recv()[3:])
+        while(True):
+            pass
         ws.close()
-    elif sys.argv[1] == "login":
+    elif sys.argv[flags_start] == "login":
         ws = create_connection(server)
         try:
             uidfile = open(filename, "rt")
@@ -103,5 +119,67 @@ else:
         uid = int(uid)
         print("Logging in as " + str(uid))
         ws.send("LGN" + str(uid))
+        while(True):
+            pass
+    elif sys.argv[flags_start] == "info":
+        ws = create_connection(server)
+        try:
+            uidfile = open(filename, "rt")
+            uid = uidfile.readline().rstrip()
+        except FileNotFoundError:
+            print("hah")
+            print ("REQ")
+            ws.send("REQ")
+            print ("Sent")
+            print ("Reeiving...")
+            result =  ws.recv()[3:]
+            jr = json.loads(result) 
+            print (jr)
+            for key in list(jr.keys()):
+                prettyKey = key.replace("_", " ")
+                prettyKey = prettyKey.title()
+                print(prettyKey + ": ")
+                jr[key] = input()
+                
+            print (jr)
+            ws.send("USR"+json.dumps(jr))
+            uid = ws.recv()[3:]
+            print (uid)
+            uidfile = open(filename, "w+")
+            uidfile.write(str(uid))
+        uid = int(uid)
+        print("Getting info for " + str(uid))
+        ws.send("INF" + str(uid))
+        while(True):
+            pass
+    elif sys.argv[flags_start] == "list":
+        ws = create_connection(server)
+        try:
+            uidfile = open(filename, "rt")
+            uid = uidfile.readline().rstrip()
+        except FileNotFoundError:
+            print("hah")
+            print ("REQ")
+            ws.send("REQ")
+            print ("Sent")
+            print ("Reeiving...")
+            result =  ws.recv()[3:]
+            jr = json.loads(result) 
+            print (jr)
+            for key in list(jr.keys()):
+                prettyKey = key.replace("_", " ")
+                prettyKey = prettyKey.title()
+                print(prettyKey + ": ")
+                jr[key] = input()
+                
+            print (jr)
+            ws.send("USR"+json.dumps(jr))
+            uid = ws.recv()[3:]
+            print (uid)
+            uidfile = open(filename, "w+")
+            uidfile.write(str(uid))
+        uid = int(uid)
+        print("Getting info for " + str(uid))
+        ws.send("LST" + str(uid))
         while(True):
             pass
