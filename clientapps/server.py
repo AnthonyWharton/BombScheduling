c68f5db5394ae2322845da52c96db4b6ce0156f2
Sean <sean.r.innes@googlemail.com>
2017-03-12 10:20:23 +0000

    updates

--- clientapps/server.py
+++ clientapps/server.py
@@ -78,19 +78,19 @@ def dostuff(msg):
     p.stop()
     print("Tk exited")
 
-# ws = create_connection("ws://139.59.162.84:40111")
-# print("Connected")
-# ws.send("LGN" + sys.argv[1])
-# print("Logged in")
-# while(True):
-#    mess = ws.recv()
-#    print(mess)
-#    if mess[:3] == "ALR":
-#        dostuff(mess[3:])
+ws = create_connection("ws://139.59.162.84:40111")
+print("Connected")
+ws.send("LGN" + sys.argv[1])
+print("Logged in")
+while(True):
+   mess = ws.recv()
+   print(mess)
+   if mess[:3] == "ALR":
+       dostuff(mess[3:])
 
 
 # dostuff("This is the message")
 # dostuff("apple pie")
 
-dostuff("{\"title\": \"Apples\", \"body\": \"fiji\"}")
+# dostuff("{\"title\": \"Apples\", \"body\": \"fiji\"}")
 # dostuff("{\"title\": \"Apples\", \"body\": \"fiji\"}")
